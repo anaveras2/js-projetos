912f48345823242610400032f8ae7caacc21de9b
ana luiza <00001128972955sp@al.educacao.sp.gov.br>
2024-11-25 00:07:29 -0300

    alterando códigos dos arquivos js, css e html

--- sketch.js
+++ sketch.js
@@ -1,18 +1,53 @@
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
 function setup() {
-    createCanvas(600, 600);
-     background("white");
-  }
-  
-  
-  function draw() {
-    
-    stroke ("blue");
-    fill ("red");
-   
-    
-    // console.log(mouseIsPressed);
-    
-    if (mouseIsPressed){
-      rect(mouseX, mouseY, 20, 35);
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Cobra Kai";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "Convenção das Bruxas";
+        } else {
+          return "A História sem Fim";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
     }
   }
+}
